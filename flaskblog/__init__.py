1fd94b7d002a3345eda881bd97631052014a965a
NoOneWillSeeIt <rest338@gmail.com>
2019-06-29 19:02:52 +0800

    end of 7th lesson

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,5 +9,9 @@ app.config['SECRET_KEY'] = '4dc9320e402e8c7ea6b29c5a4f7f889e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
 	"""docstring for RegistrationForm"""
@@ -13,6 +16,16 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('That username is already taken!')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('That email is already taken!')	
+
 
 class LoginForm(FlaskForm):
 	"""docstring for LoginForm"""
@@ -23,3 +36,25 @@ class LoginForm(FlaskForm):
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
 
+
+class UpdateAccountForm(FlaskForm):
+	"""docstring for RegistrationForm"""
+
+	username = StringField('Username', 
+							validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('E-mail',
+						validators=[DataRequired(), Email()])
+	picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('That username is already taken!')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('That email is already taken!')	
